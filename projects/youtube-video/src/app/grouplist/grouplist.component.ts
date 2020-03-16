47cdf375a38d4ebdce9aa7ce42ee39f57377a9a8
ajits11 <sawant.ajit11@gmail.com>
2020-03-16 18:28:00 +0530

    Youtube video

--- projects/youtube-video/src/app/grouplist/grouplist.component.ts
+++ projects/youtube-video/src/app/grouplist/grouplist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-grouplist',
   templateUrl: './grouplist.component.html',
@@ -7,17 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GrouplistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {
 
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/c9F5kMUfFKk");
+   }
+ // vUrl="https://www.youtube.com/watch?v=mJaxCjNJDww";
+  safeSrc: SafeResourceUrl;
   ngOnInit(): void {
   }
   listofvideos=[
-    {'title':'test','url':'https://www.youtube.com/watch?v=mJaxCjNJDww'},
-    {'title':'test1','url':'https://www.youtube.com/watch?v=6GMAugzV5ls'},
-    {'title':'test2','url':'https://www.youtube.com/watch?v=WcrIjf0Ps6E'},
-    {'title':'test3','url':'https://www.youtube.com/watch?v=tFoUuFq3vHw'}
-  
-  
+     {'title':'test','url':'https://www.youtube.com/embed/mJaxCjNJDww'},
+    {'title':'test1','url':'https://www.youtube.com/embed/6GMAugzV5ls'},
+    {'title':'test2','url':'https://www.youtube.com/embed/WcrIjf0Ps6E'},
+    {'title':'test3','url':'https://www.youtube.com/embed/tFoUuFq3vHw'}
   ];
+
+  playVideo(videourl){
+    this.safeSrc=this.sanitizer.bypassSecurityTrustResourceUrl(videourl);
+  }
 
 }
